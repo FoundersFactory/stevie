b6c2e8524a3af7f85b3d2111d38ac608d0766dc0
Phillip Caudell <phill@foundersfactory.co>
2016-12-03 16:48:25 +0000

    Adds camera image and image capture

---
+++ ios/Stevie/Stevie/CameraView.swift
@@ -0,0 +1,66 @@
+//
+//  CameraView.swift
+//  Stevie
+//
+//  Created by Phillip Caudell on 03/12/2016.
+//  Copyright © 2016 Founders Factory. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol CameraViewDelegate {
+    
+    func cameraView(_:CameraView, image: UIImage)
+}
+
+class CameraView: UIView, AVCapturePhotoCaptureDelegate {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    let stillImageOutput = AVCaptureStillImageOutput()
+    var delegate: CameraViewDelegate?
+    
+    func start() {
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetMedium
+        
+        do {
+            let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            let captureDeviceInput = try AVCaptureDeviceInput.init(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            
+            print("Unable to start camera")
+        }
+        
+        stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+        captureSession.addOutput(stillImageOutput)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        self.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+        
+    }
+    
+    override func layoutSubviews() {
+        previewLayer?.frame = self.bounds
+    }
+
+    func requestImage() {
+    
+        if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
+            
+            stillImageOutput.captureStillImageAsynchronously(from: videoConnection) {
+                (imageDataSampleBuffer, error) -> Void in
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                let image = UIImage(data: imageData!)
+                self.delegate?.cameraView(self, image: image!)
+            }
+        }
+    }
+}
+
--- ios/Stevie/Stevie/ViewController.swift
+++ ios/Stevie/Stevie/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CameraViewDelegate {
+    
+    let cameraView = CameraView()
+    let imagePreview = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(cameraView)
+        cameraView.delegate = self
+        cameraView.start()
+        
+        self.view.backgroundColor = UIColor.white
+        
+        let gesture = UITapGestureRecognizer(target: self, action:#selector(self.capture))
+        self.view.addGestureRecognizer(gesture)
+        
+        imagePreview.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.view.addSubview(imagePreview)
+        imagePreview.backgroundColor = UIColor.red
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        cameraView.frame = self.view.bounds
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func capture() {
+        cameraView.requestImage()
+    }
+    
+    func cameraView(_: CameraView, image: UIImage) {
+        print("got an image!")
+        print(image)
     }
 }
 
