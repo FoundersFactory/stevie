b0e4deb7f3c6b9f3278089b2f122510fd82c7f08
Paul Egan <paulegan@mail.com>
2016-12-03 20:33:24 +0000

    Hooked up Watson API to Stevie /find endpoint

--- api/app.py
+++ api/app.py
@@ -1,24 +1,42 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request
+from StringIO import StringIO
+from watson import classify
+
+
 app = Flask(__name__)
 
-magic = 0.0
 
 @app.route('/')
 def index():
     return jsonify(hello='world')
 
+
 @app.route('/find', methods=['POST'])
 def find():
-    global magic
-
-    item_class = request.args.get('class', None)
-    if not item_class:
+    object_class = request.args.get('class', None)
+    if not object_class:
         return jsonify(error=True, message='Missing class paramater'), 400
 
     image_data = request.stream.read()
     if len(image_data) < 1:
         return jsonify(error=True, message='Missing image data'), 400
+    else:
+        image_data = StringIO(image_data)
+        # Force jpeg - Watson API needs name to map mimetype:
+        image_data.name = 'temp.jpg'
 
-    magic = (magic + 0.1) % 1
+    try:
+        result = classify(image_data, object_class=object_class)
+    except AttributeError:
+        return jsonify(error=True, message='Invalid image data?'), 400
+    else:
+        try:
+            # There's only one image sent and only one classifier specified,
+            # so checking index 0 should be safe here:
+            classifiers = result['images'][0]['classifiers']
+            score = classifiers[0]['classes'][0]['score']
+        except IndexError:
+            # The classifier didn't match
+            score = 0.0
 
-    return jsonify(**{ 'score': magic, 'class': item_class })
+    return jsonify(**{'score': score, 'class': object_class})
