5ab61c89d223f9a9d7c494c3f403aa64ce60384a
Phillip Caudell <phill@foundersfactory.co>
2016-12-03 22:11:36 +0000

    Adds speech utterence lol

--- ios/Stevie/Stevie/AppController.swift
+++ ios/Stevie/Stevie/AppController.swift
@@ -9,37 +9,53 @@
 import UIKit
 import SpeechToTextV1
 import Alamofire
+import AVFoundation
 
 class AppController {
     
     static let sharedController = AppController()
     
-    init() {
-    
-    }
-    
-    func startListening() {
+    func listen(completion:@escaping(_ result: String?, _ didUnderstand: Bool) -> (Void)) {
         
         let speechToText = SpeechToText(username: "34fa8261-6833-44b2-b43d-8120ae84a9af", password: "ek56Y1oTOv2w")
         var settings = RecognitionSettings(contentType: .opus)
         settings.continuous = false
         settings.interimResults = false
         
+        // For our demo, we only want to match these words
+        settings.keywords = ["card", "chocolate", "keys", "oreo"]
+        settings.keywordsThreshold = 0.1
+        
         let failure = { (error: Error) in print(error) }
         speechToText.recognizeMicrophone(settings: settings, failure: failure) { results in
+            
             print(results.bestTranscript)
+            let keyword = results.results.first?.keywordResults?.first?.key
+
+            if keyword != nil {
+                completion(keyword!, true)
+            } else {
+                completion(nil, false)
+            }
         }
     }
     
     func detect(item: String, image: UIImage, completion:@escaping (_ confidence: Float) -> (Void)) {
         
         let data = UIImageJPEGRepresentation(image, 0.8)
-        Alamofire.upload(data!, to: "http://ec2-52-50-132-122.eu-west-1.compute.amazonaws.com:8000/find?class=card").responseJSON { response in
+        Alamofire.upload(data!, to: "http://ec2-52-50-132-122.eu-west-1.compute.amazonaws.com:8000/find?class="+item).responseJSON { response in
 
             let dic = response.result.value as! Dictionary<String, Any>
             completion(dic["score"] as! Float)
-        
         }
+    }
+    
+    func speak(text: String, pitch: Float) {
+    
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.pitchMultiplier = pitch
         
+        let synth = AVSpeechSynthesizer()
+        synth.speak(utterance)
     }
 }
--- ios/Stevie/Stevie/ViewController.swift
+++ ios/Stevie/Stevie/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, CameraViewDelegate {
     let overlayView = OverlayView()
     var actionButton: UIButton?
     var testLabel = UILabel()
+    var lookingForItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +34,11 @@ class ViewController: UIViewController, CameraViewDelegate {
         
         actionButton = UIButton(type: .system)
         actionButton?.setImage(#imageLiteral(resourceName: "actionButton"), for: .normal)
-        actionButton?.addTarget(self, action: #selector(self.capture), for: .touchUpInside)
+        actionButton?.addTarget(self, action: #selector(self.startListening), for: .touchUpInside)
         self.view.addSubview(actionButton!)
         
         testLabel.text = "Hello"
-        testLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        testLabel.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
         testLabel.textColor = UIColor.red
         testLabel.backgroundColor = UIColor.black
         self.view.addSubview(testLabel)
@@ -57,15 +58,57 @@ class ViewController: UIViewController, CameraViewDelegate {
     }
     
     func cameraView(_: CameraView, image: UIImage) {
-
-        AppController.sharedController.detect(item: "cat", image: image, completion:{(confidence: Float) -> (Void) in
+        
+        if lookingForItem == nil {
+            return
+        }
+        
+        AppController.sharedController.detect(item: lookingForItem!, image: image, completion:{(confidence: Float) -> (Void) in
             print(confidence)
             self.testLabel.text = confidence.description
+            
+            if confidence < 0.2 {
+                AppController.sharedController.speak(text: "Cold", pitch: 1)
+            } else if confidence < 0.3 {
+                AppController.sharedController.speak(text: "Warm", pitch: 1)
+            } else if confidence < 0.6 {
+                AppController.sharedController.speak(text: "Warm", pitch: 1.5)
+                
+            } else if confidence < 0.7 {
+                AppController.sharedController.speak(text: "Warmer", pitch: 2)
+            } else if confidence > 0.71 {
+                AppController.sharedController.speak(text: "Found it!", pitch: 1)
+                self.testLabel.text = "Found it!"
+                self.lookingForItem = nil
+            }
+            
+            
+            if confidence < 0.7 {
+                self.capture()
+            }
+            
+            
         })
     }
     
     func startListening() {
         
+        AppController.sharedController.listen { (result, didUnderstand) -> (Void) in
+            
+            self.testLabel.text = result
+            
+            if didUnderstand {
+                
+                self.testLabel.text = "Looking for " + result!
+                self.lookingForItem = result!
+                self.capture()
+                
+            } else {
+                self.testLabel.text = "I don't understand"
+            }
+            
+            AppController.sharedController.speak(text: self.testLabel.text!, pitch: 1)
+        }
     }
 }
 
